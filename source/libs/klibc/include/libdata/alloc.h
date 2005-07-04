382d63600f5e47275c5b6e695b89ba0be802b8ad
Christophe Eymard <eymard_c@epita.fr>
2005-07-04 14:55:25 +0000

    improvement for malloc. They still do nothing.

--- source/libs/klibc/include/libdata/alloc.h
+++ source/libs/klibc/include/libdata/alloc.h
@@ -1,27 +1,32 @@
 /*
  * copyright quintard julien
- * 
+ *
  * kaneton
- * 
+ *
  * malloc.h
- * 
+ *
  * path          /home/mycure/kaneton
- * 
+ *
  * made by mycure
  *         quintard julien   [quinta_j@epita.fr]
- * 
+ *
  * started on    Fri Feb 11 02:32:39 2005   mycure
+<<<<<<< .mine
+** Last update Sun Jul  3 23:25:23 2005 Christophe Eymard
+=======
  * last update   Mon Jul  4 14:28:40 2005   mycure
+>>>>>>> .r138
  */
 
 #ifndef LIBDATA_ALLOC_H
-#define LIBDATA_ALLOC_H		1
+# define LIBDATA_ALLOC_H       	1
 
 /*
  * ---------- dependencies ----------------------------------------------------
  */
 
-#include <klibc/include/libsys/types.h>
+# include <klibc/include/libsys/types.h>
+# include "../../../../core/include/arch/machdep/machdep.h"
 
 /*
  * ---------- types -----------------------------------------------------------
@@ -31,22 +36,41 @@
  * chunck
  */
 
-typedef struct			s_chunck
+# define ALLOC_FIRST_FIT
+
+# ifdef ALLOC_FIRST_FIT
+
+# define ALLOC_FREE		0x1
+# define ALLOC_LAST		0x2
+
+/*
+** The big, ugly and apparently useless "align" field is actually necessary to
+** ensure that the size of the s_chunk structure is a multiple of the size of a
+** pointer.
+*/
+struct				s_chunk
 {
   size_t			size;
-
+  unsigned int			flags;
   struct s_chunck*		prv;
-  struct s_chunck*		nxt;
-}				t_chunck;
+  char				align[sizeof (t_vaddr) - ((sizeof (size_t) +
+							sizeof (unsigned int) +
+							sizeof (t_vaddr)) %
+						       sizeof (t_vaddr))];
+} __attribute__ ((__packed__));
+
+typedef struct s_chunk t_chunk;
 
 /*
  * alloc global information
  */
-
 typedef struct
 {
-  t_chunck*			alloc;
-  t_chunck*			free;
+  size_t			size;
+  size_t			heap; /* Limit of the heap */
+  t_chunk*			first_blk;
+  t_vaddr			addr;
 }				t_alloc;
+# endif
 
 #endif
--- source/libs/klibc/libdata/alloc.c
+++ source/libs/klibc/libdata/alloc.c
@@ -1,18 +1,26 @@
 /*
  * copyright quintard julien
- * 
+ *
  * kaneton
- * 
+ *
  * malloc.c
- * 
+ *
  * path          /home/mycure/kaneton
- * 
+ *
  * made by mycure
  *         quintard julien   [quinta_j@epita.fr]
- * 
+ *
  * started on    Fri Feb 11 02:50:21 2005   mycure
- * last update   Sun Jul  3 22:39:32 2005   mycure
+** Last update Sun Jul  3 23:35:23 2005 Christophe Eymard
  */
+
+/*
+ * This macro aligns an address to a multiple of the size of a long int.
+ * it is used to ensure the processor uses the minimal number of cycles
+ * to fetch the adress.
+ * This macro shall be used in both implementations.
+ */
+#define __align(Addr)  (Addr) + sizeof (t_vaddr) - (((t_vaddr) Addr + 1) % sizeof (t_vaddr) - 1)
 
 /*
  * ---------- information -----------------------------------------------------
@@ -25,6 +33,7 @@
  */
 
 #include <klibc/include/klibc.h>
+#include <klibc/include/libdata/alloc.h>
 
 /*
  * ---------- globals ---------------------------------------------------------
@@ -36,15 +45,107 @@ t_alloc			alloc;
  * ---------- functions -------------------------------------------------------
  */
 
+#ifdef ALLOC_FIRST_FIT
+
+/*
+ * Here follows a really basic implementation of the first fit algorithm, known
+ * for its extreme slowness. The only reason it was implemented here was to
+ * have a malloc to pursue developpment.
+ * Later, a buddy system algorithm shall be implemented.
+ */
+
+# define __next_chunk(Chunk) (void *) ((t_vaddr) Chunk + Chunk->size)
+# define __prev_chunk(Chunk) (void *) (Chunk->prv)
+
+/*!
+** @brief 	Segments a contiguous zone of allocation
+**
+**   Here we take a chunk, make it of size "size", and make a new
+**   chunk right after it. It is the contrary of merge.
+**
+** @param chunk		The chunk we want to fragment
+** @param size		The size we want it to be
+**
+** @return 0 if all went smoothly, 1 otherwise.
+*/
+static int		fragment(t_chunk *chunk, size_t size)
+{
+  t_chunk		*new = NULL;
+  size_t		size_save = size;
+
+  if (!(chunk->flags & ALLOC_FREE))
+    return 1;
+  chunk->size = __align(size);
+  new = __next_chunk(chunk);
+  new->size = size_save - sizeof (t_chunk) - size;
+  new->flags = ALLOC_FREE;
+  if (chunk->flags & ALLOC_LAST)
+  {
+    chunk->flags &= ~(ALLOC_LAST);
+    new->flags |= ALLOC_LAST;
+  }
+  return 0;
+}
+
+/*!
+** @brief Merge free chunks
+**
+** @param chunk		The starting chunk we want to merge.
+*/
+static void		merge(t_chunk chunk)
+{
+
+}
+
 void*			malloc(size_t				size)
 {
 
 }
 
+/*!
+** @brief Initialiaze the allocation structure.
+**
+** The initialisation is done in two passes.
+** 1) It inits the main structure to contain : the address of the
+**   first block, the whole size, the adress of the whole free space,
+**   and the adress of the heap (useful if we want to modify the space later on)
+**
+** 2) It initialises the first (and last) block, filling it with the size of
+**   the space it contains, the flags, and the location of the previous block,
+**   which is zero, since there isn't any yet.
+**
+**   Now, we're ready to rock.
+**   Folks, remember that for the sake of optimization, we try to keep all the
+**   addresses aligned, so don't mind the funky pointer arithmetics.
+**
+** @param addr	The adress of the beginning of the heap
+** @param size	The size of it all
+**
+** @return	zero. I don't even understand why I should say that it didn't
+** 		went smooth.
+*/
 int			alloc_init(t_vaddr			addr,
 				   t_size			size)
 {
   memset(&alloc, 0x0, sizeof(t_alloc));
 
-  // XXX alloc.free = alloc_add(alloc.free, addr, size);
+  /*
+   * 1)
+   */
+  alloc.addr = addr;
+  alloc.first_blk = (t_chunk*) __align(addr);
+  alloc.size = size;
+  alloc.heap = addr + sizeof (t_chunk);
+
+  /*
+   * 2)
+   */
+  alloc.first_blk->size = size - sizeof (t_chunk) -
+    (addr - (t_vaddr) alloc.first_blk);
+  alloc.first_blk->flags = ALLOC_FREE | ALLOC_LAST;
+  alloc.first_blk->prv = NULL;
+
+  return 0;
 }
+
+#endif
