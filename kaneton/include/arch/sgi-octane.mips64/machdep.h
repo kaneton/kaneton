9a3d452b3ec738cd6a9b1ec0df21e73122443898
Enguerrand Raymond <enguerrand.raymond@epita.fr>
2006-10-15 20:06:57 +0000

    Adding machdep.h in include/arch/sgi... folder.

---
+++ kaneton/include/arch/sgi-octane.mips64/machdep.h
@@ -0,0 +1,23 @@
+/*
+ * ---------- header ----------------------------------------------------------
+ *
+ * project       kaneton
+ *
+ * license       kaneton
+ *
+ * file          /home/enguerrand/kaneton/kaneton/include/arch/sgi-octane.mips64/machdep.h
+ *
+ * created       Enguerrand RAYMOND   [sun oct 15 22:04:48 2006]
+ * updated       Enguerrand RAYMOND   [sun oct 15 22:06:04 2006]
+ */
+
+#ifndef OCTANE_MACHDEP_H
+#define OCTANE_MACHDEP_H	1
+
+/*
+ * ---------- includes --------------------------------------------------------
+ */
+
+#include <liboctane.h>
+
+#endif
