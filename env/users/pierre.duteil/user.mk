9987a68a32bcff25562e84e97744d7444d2c36c0
Matthieu Bucchianeri <matthieu.bucchianeri@epita.fr>
2007-02-06 20:54:40 +0000

    added profile for julian and pierre

---
+++ env/users/pierre.duteil/user.mk
@@ -0,0 +1,23 @@
+##
+## licence       kaneton licence
+##
+## project       kaneton
+##
+## file          /home/buckman/kaneton/env/users/julian.pidancet/user.mk
+##
+## created       matthieu bucchianeri   [mon jan 30 23:32:09 2006]
+## updated       matthieu bucchianeri   [tue feb  6 21:57:07 2007]
+##
+
+#
+# ---------- information ------------------------------------------------------
+#
+# this file contains specific user makefile dependencies, rules, variables
+# etc..
+#
+
+# disable checks
+_CHECK_LO_	:=
+
+# for using gdb with qemu
+_CFLAGS_	+=
