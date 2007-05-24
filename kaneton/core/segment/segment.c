14fa63d3424eec7413144cc6780da549d6180cef
Matthieu Bucchianeri <matthieu.bucchianeri@epita.fr>
2007-05-24 14:30:54 +0000

    fixed bug on segment_clone

--- kaneton/core/segment/segment.c
+++ kaneton/core/segment/segment.c
@@ -233,7 +233,7 @@ t_error			segment_clone(i_as			asid,
    * 2)
    */
 
-  if (segment_reserve(asid, from->size, PERM_WRITE, new) != ERROR_NONE)
+  if (segment_reserve(asid, from->size, PERM_READ | PERM_WRITE, new) != ERROR_NONE)
     SEGMENT_LEAVE(segment, ERROR_UNKNOWN);
 
   /*
