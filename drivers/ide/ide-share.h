798e0d86857990d7012ec5b2fd85767dd9446864
Nicolas Grandemange <nicolas.grandemange@epita.fr>
2008-02-06 02:10:06 +0000

    Missing file

---
+++ drivers/ide/ide-share.h
@@ -0,0 +1,71 @@
+#ifndef IDE_SHARE_H_
+#define IDE_SHARE_H_
+
+/*
+ * ---------- dependencies ----------------------------------------------------
+ */
+
+#include <libkaneton.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <crt.h>
+
+/*
+ * ---------- macros ----------------------------------------------------------
+ */
+
+/*
+ * service name.
+ */
+
+#define DRIVER_IDE_NAME			"ide"
+
+/*
+ * service operations.
+ */
+
+#define IDE_DRIVER_REQ_READ		1
+#define IDE_DRIVER_REP_READ		2
+#define IDE_DRIVER_REP_ERR		3
+
+/*
+ * message type description.
+ */
+
+#define MESSAGE_TYPE_DRIVER_IDE	(MESSAGE_TYPE_CRT + 8)
+#define MESSAGE_SIZE_DRIVER_IDE	(sizeof (t_driver_ide_message))
+
+/*
+ * ide buffer size.
+ */
+
+#define IDE_DRIVER_MAX		100
+
+
+/*
+ * ---------- types -----------------------------------------------------------
+ */
+
+typedef struct
+{
+  t_id		operation;
+  union
+  {
+    struct
+    {
+      uint8_t	ctr;
+      uint8_t	dev;
+    }		req_read;
+    struct
+    {
+      char	buf[IDE_DRIVER_MAX];
+    }		rep_read;
+    struct
+    {
+      char	text[IDE_DRIVER_MAX];
+    }		rep_err;
+  } u;
+}			t_driver_ide_message;
+
+#endif
