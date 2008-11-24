cf4335f634b29d667d7030a88e6197746740c065
Francois Goudal <francois.goudal@epita.fr>
2008-11-24 10:26:53 +0000

    [export/test.yml]   o added sample for freplace module
    [export/data]
      o folder aimed to store the replacement files for the export
    
    [export/data/Makefile]
      o replacement file sample
    
    [tool/export/modules/freplace.py]
      o new file replacement module for export

---
+++ tool/export/modules/freplace.py
@@ -0,0 +1,45 @@
+#
+# ---------- header -----------------------------------------------------------
+#
+# project       kaneton
+#
+# license       kaneton
+#
+# file          /Users/francois/kaneton/tool/export/modules/freplace.py
+#
+# created       francois goudal   [sat oct 25 20:57:38 2008]
+# updated       francois goudal   [wed nov 12 05:26:29 2008]
+#
+
+#
+# ---------- imports ----------------------------------------------------------
+#
+
+import env
+
+#
+# ---------- functions --------------------------------------------------------
+#
+
+#
+# module_init()
+#
+# called by export.py on startup, to discover the available actions
+# returns an array of strings, the index 0 is the action name, and the
+# others are the parameters required in the YAML file.
+#
+def module_init():
+    return ['freplace','src','dst']
+
+#
+# module_action(export_dir, arg)
+#
+# called by export.py when required by the YAML file.
+# this function does the job of the action.
+# arg contains the parameters required as specified in the module_init return
+# value.
+#
+def module_action(export_dir, arg):
+    env.display(env.HEADER_OK, 'action freplace ' + arg['src'] + ' to ' + arg['dst'], env.OPTION_NONE)
+    env.copy(export_dir + '/' + arg['src'], export_dir + '/' + arg['dst'], env.OPTION_NONE)
+    return 0
